2c4d4d2e5c7e84eef482cd464cd34acb5d2565d1
Christopher <cvald016@fiu.edu>
2017-05-30 17:33:46 -0400

    rest of files

---
+++ db/migrate/20170530170607_create_projects.rb
@@ -0,0 +1,12 @@
+class CreateProjects < ActiveRecord::Migration[5.1]
+  def change
+    create_table :projects do |t|
+      t.belongs_to :user, foreign_key: true
+      # t.references :user, index: true, foreign_key: true
+      t.string :source
+      t.string :url
+
+      t.timestamps
+    end
+  end
+end
